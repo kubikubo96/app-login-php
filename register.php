2d38ee217735efc78e03d42d03d9f2f7cd0f58a1
KubiKubo <tien.nguyentat.1@gmail.com>
2019-10-06 03:25:25 +0700

    1

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>register</title>
+    <title>Title</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
@@ -14,46 +14,34 @@ session_start();
 <?php
 include_once "config.php";
 if (isset($_POST) && !empty($_POST)) {
-
     $errors = array();
-
     if (!isset($_POST["username"]) || empty($_POST["username"])) {
         $errors[] = "Username không hợp lệ";
     }
-
     if (!isset($_POST["password"]) || empty($_POST["password"])) {
         $errors[] = "password không hợp lệ";
     }
-
     if (!isset($_POST["confirm_password"]) || empty($_POST["confirm_password"])) {
         $errors[] = "confirm password không hợp lệ";
     }
-
     if ($_POST["confirm_password"] !== $_POST["password"]) {
         $errors[] = "Confirm password khác password";
     }
-
-
     if (empty($errors)) {
         /**
          * Nếu không có lỗi thì thực thi câu lệnh insert vào CSDL
+         * md5 là để khi insert password vào sql thì nó sẽ mã hóa
          */
         $username = $_POST["username"];
         $password = md5($_POST["password"]);
         $created_at = date("Y-m-d H:i:s");
-
         $sqlInsert = "INSERT INTO users (username, password, created_at) VALUES (?,?,?)";
-
         // Chuẩn bị cho phần SQL
         $stmt = $connection->prepare($sqlInsert);
-
         // Bind 3 biến vào trong câu SQL
         $stmt->bind_param("sss", $username, $password, $created_at);
-
         $stmt->execute();
-
         $stmt->close();
-
         echo "<div class='alert alert-success'>";
         echo "Đăng ký người dùng mới thành công . Hãy <a href='login.php'>đăng nhập</a> ngay lập tức";
         echo "</div>";
