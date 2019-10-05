adf28bf218ab93008de8d3773fa027e66a5123f9
KubiKubo <tien.nguyentat.1@gmail.com>
2019-10-06 04:18:25 +0700

    1

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 define("DB_SERVER", "us-cdbr-iron-east-05.cleardb.net");
 define("DB_SERVER_USERNAME", "bd910bb52ff3e1");
 define("DB_SERVER_PASSWORD", "9a702567");
-define("DB_SERVER_NAME", "heroku_0cbe89d374a6f92");
+define("DB_SERVER_NAME", "heroku_d3411b8e0a33943");
 
 /**
  * mysqli_connect() kết nối đến CSDL
